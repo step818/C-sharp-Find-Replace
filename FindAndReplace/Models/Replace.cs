eed2d855dc1d59a1544f5be7ce9161f77d87c3e0
Stephen Trewick <stephent818@gmail.com>
2019-10-02 17:57:10 -0700

    failing test on NewPhrase

--- FindAndReplace/Models/Replace.cs
+++ FindAndReplace/Models/Replace.cs
@@ -15,7 +15,26 @@ namespace FindAndReplace.Models
     {
       string uPhrase = InheritedInfo.Paragraph;
       string uWord = InheritedInfo.Word;
-      string result = "this works";
+      string result = "";
+      for(int i = 0, j = 0; i < uPhrase.Length; i++)
+      {
+        if(uPhrase[i] == uWord[j])
+        {
+          if(j == uWord.Length - i)
+          {
+            result += Name;
+            j = 0; // reset
+          }
+          else
+          {
+            j++;
+          }
+        }
+        else
+        {
+          result += uPhrase[i];
+        }
+      }
       return result;
     }
   }
